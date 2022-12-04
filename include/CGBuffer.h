1222062ef9ae19b8a5e33567df76b4b8d93db91c
ycshen1009 <ohg5d6@gmail.com>
2022-12-04 16:20:45 -0500

    CGBuffer update

--- include/CGBuffer.h
+++ include/CGBuffer.h
@@ -1,320 +1,394 @@
-#pragma once
-
-#include <cuda.h>
-
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <stdio.h>
-#include <cuda.h>
-#include <cmath>
-
-#include <glm/glm.hpp>
-#include <device_launch_parameters.h>
-#include <cuda_runtime.h>
-
-class CGBufferBase {
-protected:
-	uint32_t m_bufferSize;
-	uint32_t m_appendSize;
-
-	std::string bufferNmae;
-	bool isMalloc;
-
-	void* m_rawPtr;
-	void* m_devicePtr;
-
-public:
-
-	CGBufferBase()
-		: CGBufferBase(0, "Autonomy: " + std::to_string(0))
-	{
-
-	}
-
-	CGBufferBase(int size)
-		: CGBufferBase(size, "Autonomy: " + std::to_string(0)) {}
-
-	CGBufferBase(int size, std::string name)
-		: m_bufferSize(size), bufferNmae(name), isMalloc(false), m_rawPtr(nullptr), m_devicePtr(nullptr)
-	{
-
-	}
-
-	virtual uint32_t typeSize() = 0;
-	//virtual void setDevicePtr(void* rawPtr) = 0;
-
-	void* getRawPtr() { return m_rawPtr; }
-
-	void reallocationHost(int appendSize) {
-
-		std::vector<glm::vec3> vec(appendSize, glm::vec3(0.f));
-		std::vector<glm::vec3>* p = ((std::vector<glm::vec3>*)m_rawPtr);
-		p->insert(p->end(), vec.begin(), vec.end());
-
-		m_appendSize = appendSize;
-		m_bufferSize += appendSize;
-	}
-
-	void reallocationDevice(int appendSize) {
-		void* devicePtr;
-		cudaMalloc((void**)&devicePtr, (m_bufferSize + appendSize) * typeSize());
-		cudaMemcpy(devicePtr, m_devicePtr, m_bufferSize * typeSize(), cudaMemcpyDeviceToDevice);
-
-		cudaFree(m_devicePtr);
-
-		m_devicePtr = devicePtr;
-		m_appendSize = appendSize;
-		m_bufferSize += appendSize;
-	}
-
-	uint32_t getSize() {
-		return m_bufferSize;
-	}
-
-	void* getDevicePtr() { return m_devicePtr; }
-
-	uint32_t getAppendSize() {
-		return m_appendSize;
-	}
-
-	void setSize(uint32_t size) {
-		m_bufferSize = size;
-	}
-
-	bool hasMemory() {
-		return isMalloc;
-	}
-
-	std::string getName() {
-		return bufferNmae;
-	}
-
-	void setName(std::string name) {
-		bufferNmae = name;
-	}
-
-	virtual ~CGBufferBase() {};
-};
-
-template<class T>
-class CGBuffer : public CGBufferBase {
-private:
-	std::vector<T> m_data;
-
-public:
-	CGBuffer()
-		: CGBufferBase()
-	{
-	}
-
-	CGBuffer(int size, T value)
-		: CGBuffer("", size, value)
-	{
-	}
-
-	CGBuffer(std::string name, int size, T value)
-		: CGBufferBase(size, name),
-		m_data(std::vector<T>(size, value))
-	{
-		m_rawPtr = &m_data;
-	}
-
-	uint32_t typeSize() { return sizeof(T); }
-
-	void addVlaue(T value) {
-		m_data.push_back(value);
-		m_bufferSize = m_data.size();
-	}
-
-	void copy(CGBuffer<T>* object) {
-		std::vector<T> vec = object->getData();
-		m_data = std::vector<T>(vec.begin(), vec.end());
-
-		m_bufferSize = object->getSize();
-
-		if (object->hasMemory()) {
-			this->malloc();
-			this->loadFromDevice(object->getDevicePointer(), -1, false);
-		}
-	}
-
-	CGBuffer<T>* clone() {
-		CGBuffer<T>* object = new CGBuffer<T>();
-
-		object->copy(this);
-
-		return object;
-	}
-
-	T* getRawData() {
-		return m_data.data();
-	}
-
-	std::vector<T> getData() {
-		return m_data;
-	}
-
-	void setHostData(std::vector<T>& v) {
-		m_data = std::vector<T>(v.begin(), v.end());
-	}
-
-	T* getDevicePointer() {
-		return (T*)m_devicePtr;
-	}
-
-	void setSize(int size) {
-		m_data.resize(size);
-		m_bufferSize = size;
-	}
-
-	bool malloc() {
-		isMalloc = true;
-		cudaMalloc((void**)&m_devicePtr, m_bufferSize * sizeof(T));
-
-		//m_rawPtr = m_devicePtr;
-
-		return true;
-	}
-
-	bool loadHostToDevice() {
-		cudaMemcpy(m_devicePtr, m_data.data(), m_bufferSize * sizeof(T), cudaMemcpyHostToDevice);
-		return true;
-	}
-
-	bool loadDeviceToHost() {
-		m_data.resize(m_bufferSize);
-		cudaMemcpy(m_data.data(), m_devicePtr, m_bufferSize * sizeof(T), cudaMemcpyDeviceToHost);
-		return true;
-	}
-
-	bool loadFromDevice(T* devicePtr, int N = -1, bool toHost = true) {
-		if (N == -1) {
-			N = m_bufferSize;
-		}
-		N = std::min(N, (int)m_bufferSize);
-
-		checkMalloc();
-		cudaMemcpy(m_devicePtr, devicePtr, N * sizeof(T), cudaMemcpyDeviceToDevice);
-
-		if (toHost) {
-			this->loadDeviceToHost();
-		}
-
-		return true;
-	}
-
-	bool loadToDevice(T* devicePtr, int N = -1) {
-		if (N == -1) {
-			N = m_bufferSize;
-		}
-		N = min(N, m_bufferSize);
-
-		checkMalloc();
-		cudaMemcpy(devicePtr, m_devicePtr, N * sizeof(T), cudaMemcpyDeviceToDevice);
-		return true;
-	}
-
-	void checkMalloc() {
-		if (!isMalloc) {
-			this->malloc();
-		}
-	}
-
-	static CGBufferBase* loadFromFile(std::string filename);
-	static CGBuffer<T>* loadFromFileCGBuffer(std::string filename) {
-		return dynamic_cast<CGBuffer<T>*>(loadFromFile(filename));
-	}
-
-	void clear() {
-		m_data.clear();
-		m_bufferSize = 0;
-		isMalloc = false;
-		cudaFree(m_devicePtr);
-	}
-
-	void outputObj(std::string filename = "testing.obj") {
-		std::string objContent = "g\n";
-
-		std::string type = typeid(T).name(); //struct glm::tvec3<float,0>
-		for (auto data : m_data) {
-			if (type == "struct glm::tvec3<float,0>") {
-				objContent += "v " + std::to_string(data[0]) + " " + std::to_string(data[1]) + " " + std::to_string(data[2]) + "\n";
-			}
-		}
-
-		std::ofstream myfile;
-		myfile.open(filename);
-		myfile << objContent;
-		myfile.close();
-	}
-
-	~CGBuffer() {
-		this->clear();
-	}
-};
-
-template< typename T >
-CGBufferBase* CGBuffer<T>::loadFromFile(std::string filename) {
-	char* fname = (char*)filename.c_str();
-
-	std::ifstream fp_in;
-	fp_in.open(fname);
-	if (!fp_in.is_open()) {
-		std::cout << "Error reading from file - aborting!" << std::endl;
-		throw;
-	}
-
-	std::string valueType = "";
-	getline(fp_in, valueType, '|');
-
-	std::vector<float> values;
-	while (fp_in.good()) {
-		std::string line;
-		getline(fp_in, line, ',');
-		if (!line.empty()) {
-			values.push_back(std::stof(line));
-		}
-	}
-
-	int offset = 1;
-
-	CGBufferBase* instance;
-	if (valueType == "3f") {
-		CGBuffer<glm::vec3>* o1 = new CGBuffer<glm::vec3>();
-
-		offset = 3;
-		for (int i = 0; i < values.size(); i += offset) {
-			if (valueType == "3f") {
-				o1->addVlaue(glm::vec3(values[i], values[i + 1], values[i + 2]));
-			}
-		}
-
-		o1->setSize(o1->getData().size());
-		instance = dynamic_cast<CGBufferBase*>(o1);
-		return instance;
-	}
-
-	return instance;
+/**
+* @file      main.cpp
+* @brief     Example Boids flocking simulation for CIS 565
+* @authors   Liam Boone, Kai Ninomiya, Kangning (Gary) Li
+* @date      2013-2017
+* @copyright University of Pennsylvania
+*/
+
+#include "main.hpp"
+#include "CGBuffer.h"
+#include "CGGenerator.h"
+#include <Windows.h>
+
+// ================
+// Configuration
+// ================
+
+// LOOK-2.1 LOOK-2.3 - toggles for UNIFORM_GRID and COHERENT_GRID
+#define VISUALIZE 1
+#define UNIFORM_GRID 0
+#define COHERENT_GRID 1
+
+// LOOK-1.2 - change this to adjust particle count in the simulation
+const int N_FOR_VIS = 10000;
+const float DT = 0.2f;
+#define blockSize 128
+
+/**
+* C main function.
+*/
+int main(int argc, char* argv[]) {
+  projectName = "565 CUDA Intro: Boids";
+
+  std::string filename = "../3dfield_data.txt";
+  CGBuffer<float>* posBuffer = new CGBuffer<float>("velocity", 0, 3);
+  posBuffer->initializeFromFile(filename);
+  posBuffer->setToZero();
+  posBuffer->outputObj();
+
+  CGBuffer<glm::vec3>* vbuffer = new CGBuffer<glm::vec3>("velocity", 2, glm::vec3(1.f));
+  CGBuffer<glm::vec3>* pbuffer = new CGBuffer<glm::vec3>("position", 2, glm::vec3(3.f, 4, 5));
+
+  vbuffer->malloc();
+  vbuffer->loadHostToDevice();
+
+  pbuffer->malloc();
+  pbuffer->loadHostToDevice();
+
+  ParticleGenerator::RAWDesc desc;
+  desc.direction = glm::vec3(0, 0, 1);
+  desc.speed = 2;
+  desc.size = glm::vec2(2, 2);
+  desc.deltaX = 0.5;
+  desc.center = glm::vec3(0, 0.f, 0.f);
+
+  ParticleGenerator* pg = new ParticleGenerator(desc);
+  pg->delegatePointBuffer(vbuffer);
+  pg->delegatePointBuffer(pbuffer);
+
+  // buffer->reallocationHost(3);
+
+  pbuffer->outputObj("../userOutputdata/pos_cpu_0.obj");
+
+  for (int frame = 1; frame < 10; frame++) {
+    pg->generateParticlesGPU();
+    //pg->generateParticlesCPU();
+
+    cudaDeviceSynchronize();
+
+    Boids::updatePosition(pbuffer->getSize(), pbuffer->getDevicePointer(), vbuffer->getDevicePointer());
+   // glm::vec3* pos = (pbuffer->getRawData());
+	  //glm::vec3* vec = (vbuffer->getRawData());
+	  //int size = pbuffer->getSize();
+	  //for (int i = 0; i < size; i++) {
+		 // pos[i] += vec[i];
+	  //}
+
+    std::string frameN = std::to_string(frame);
+    std::string outputObjFilePath = "../userOutputdata/pos_";
+
+    outputObjFilePath += "gpu_";
+    //outputObjFilePath += "cpu_";
+    outputObjFilePath += frameN;
+    outputObjFilePath += ".obj";
+
+    pbuffer->loadDeviceToHost();
+    pbuffer->outputObj(outputObjFilePath);
+  }
+  
+  //pg->generateParticlesGPU();
+
+  //Boids::updatePosition(pbuffer->getSize(), pbuffer->getDevicePointer(), vbuffer->getDevicePointer());
+
+  //if (init(argc, argv)) {
+  //  mainLoop();
+  //  Boids::endSimulation();
+  //  return 0;
+  //} else {
+  //  return 1;
+  //}
 }
 
-//int CGBufferBase::id = 0;
+//-------------------------------
+//---------RUNTIME STUFF---------
+//-------------------------------
 
-///**
-//* C main function.
-//*/
-//int main(int argc, char* argv[]) {
-//	std::string filename = "../geoData_example.txt";
-//	CGBuffer<glm::vec3>* posBuffer = dynamic_cast<CGBuffer<glm::vec3>*>(CGBuffer<float>::loadFromFile(filename));
-//  CGBuffer<glm::vec3>* velBuffer = new CGBuffer<glm::vec3>("buffer name", 10000, glm::vec3(1.f));
-//	posBuffer->malloc();
-//  posBuffer->loadHostToDevice();
-// 
-//  CGBuffer<glm::vec3>* posTemp = new CGBuffer<glm::vec3>();
-//	posTemp->copy(posBuffer);
-//  posTemp = posBuffer.clone();
-//
-//  posBuffer.outputObj();
-//}
+std::string deviceName;
+GLFWwindow *window;
+
+/**
+* Initialization of CUDA and GLFW.
+*/
+bool init(int argc, char **argv) {
+  // Set window title to "Student Name: [SM 2.0] GPU Name"
+  cudaDeviceProp deviceProp;
+  int gpuDevice = 0;
+  int device_count = 0;
+  cudaGetDeviceCount(&device_count);
+  if (gpuDevice > device_count) {
+    std::cout
+    << "Error: GPU device number is greater than the number of devices!"
+    << " Perhaps a CUDA-capable GPU is not installed?"
+    << std::endl;
+    return false;
+  }
+  cudaGetDeviceProperties(&deviceProp, gpuDevice);
+  int major = deviceProp.major;
+  int minor = deviceProp.minor;
+
+  std::ostringstream ss;
+  ss << projectName << " [SM " << major << "." << minor << " " << deviceProp.name << "]";
+  deviceName = ss.str();
+
+  // Window setup stuff
+  glfwSetErrorCallback(errorCallback);
+
+  if (!glfwInit()) {
+    std::cout
+    << "Error: Could not initialize GLFW!"
+    << " Perhaps OpenGL 3.3 isn't available?"
+    << std::endl;
+    return false;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(width, height, deviceName.c_str(), NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return false;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, keyCallback);
+  glfwSetCursorPosCallback(window, mousePositionCallback);
+  glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
+  glewExperimental = GL_TRUE;
+  if (glewInit() != GLEW_OK) {
+    return false;
+  }
+
+  // Initialize drawing state
+  initVAO();
+
+  // Default to device ID 0. If you have more than one GPU and want to test a non-default one,
+  // change the device ID.
+  cudaGLSetGLDevice(0);
+
+  cudaGLRegisterBufferObject(boidVBO_positions);
+  cudaGLRegisterBufferObject(boidVBO_velocities);
+
+  // Initialize N-body simulation
+  Boids::initSimulation(N_FOR_VIS);
+
+  updateCamera();
+
+  initShaders(program);
+
+  glEnable(GL_DEPTH_TEST);
+
+  return true;
+}
+
+void initVAO() {
+
+  std::unique_ptr<GLfloat[]> bodies{ new GLfloat[4 * (N_FOR_VIS)] };
+  std::unique_ptr<GLuint[]> bindices{ new GLuint[N_FOR_VIS] };
+
+  glm::vec4 ul(-1.0, -1.0, 1.0, 1.0);
+  glm::vec4 lr(1.0, 1.0, 0.0, 0.0);
+
+  for (int i = 0; i < N_FOR_VIS; i++) {
+    bodies[4 * i + 0] = 0.0f;
+    bodies[4 * i + 1] = 0.0f;
+    bodies[4 * i + 2] = 0.0f;
+    bodies[4 * i + 3] = 1.0f;
+    bindices[i] = i;
+  }
+
+
+  glGenVertexArrays(1, &boidVAO); // Attach everything needed to draw a particle to this
+  glGenBuffers(1, &boidVBO_positions);
+  glGenBuffers(1, &boidVBO_velocities);
+  glGenBuffers(1, &boidIBO);
+
+  glBindVertexArray(boidVAO);
+
+  // Bind the positions array to the boidVAO by way of the boidVBO_positions
+  glBindBuffer(GL_ARRAY_BUFFER, boidVBO_positions); // bind the buffer
+  glBufferData(GL_ARRAY_BUFFER, 4 * (N_FOR_VIS) * sizeof(GLfloat), bodies.get(), GL_DYNAMIC_DRAW); // transfer data
+
+  glEnableVertexAttribArray(positionLocation);
+  glVertexAttribPointer((GLuint)positionLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+  // Bind the velocities array to the boidVAO by way of the boidVBO_velocities
+  glBindBuffer(GL_ARRAY_BUFFER, boidVBO_velocities);
+  glBufferData(GL_ARRAY_BUFFER, 4 * (N_FOR_VIS) * sizeof(GLfloat), bodies.get(), GL_DYNAMIC_DRAW);
+  glEnableVertexAttribArray(velocitiesLocation);
+  glVertexAttribPointer((GLuint)velocitiesLocation, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boidIBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, (N_FOR_VIS) * sizeof(GLuint), bindices.get(), GL_STATIC_DRAW);
+
+  glBindVertexArray(0);
+}
+
+void initShaders(GLuint * program) {
+  GLint location;
+
+  program[PROG_BOID] = glslUtility::createProgram(
+    "shaders/boid.vert.glsl",
+    "shaders/boid.geom.glsl",
+    "shaders/boid.frag.glsl", attributeLocations, 2);
+    glUseProgram(program[PROG_BOID]);
+
+    if ((location = glGetUniformLocation(program[PROG_BOID], "u_projMatrix")) != -1) {
+      glUniformMatrix4fv(location, 1, GL_FALSE, &projection[0][0]);
+    }
+    if ((location = glGetUniformLocation(program[PROG_BOID], "u_cameraPos")) != -1) {
+      glUniform3fv(location, 1, &cameraPosition[0]);
+    }
+  }
+
+  //====================================
+  // Main loop
+  //====================================
+  void runCUDA() {
+    // Map OpenGL buffer object for writing from CUDA on a single GPU
+    // No data is moved (Win & Linux). When mapped to CUDA, OpenGL should not
+    // use this buffer
+
+    float4 *dptr = NULL;
+    float *dptrVertPositions = NULL;
+    float *dptrVertVelocities = NULL;
+
+    cudaGLMapBufferObject((void**)&dptrVertPositions, boidVBO_positions);
+    cudaGLMapBufferObject((void**)&dptrVertVelocities, boidVBO_velocities);
+
+    // execute the kernel
+    #if UNIFORM_GRID && COHERENT_GRID
+    Boids::stepSimulationCoherentGrid(DT);
+    #elif UNIFORM_GRID
+    Boids::stepSimulationScatteredGrid(DT);
+    #else
+    Boids::stepSimulationNaive(DT);
+    #endif
+
+    #if VISUALIZE
+    Boids::copyBoidsToVBO(dptrVertPositions, dptrVertVelocities);
+    #endif
+    // unmap buffer object
+    cudaGLUnmapBufferObject(boidVBO_positions);
+    cudaGLUnmapBufferObject(boidVBO_velocities);
+  }
+
+  void mainLoop() {
+    double fps = 0;
+    double timebase = 0;
+    int frame = 0;
+
+    Boids::unitTest(); // LOOK-1.2 We run some basic example code to make sure
+                       // your CUDA development setup is ready to go.
+    std::vector<double> fpsList;
+
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+
+      frame++;
+      double time = glfwGetTime();
+
+      if (time - timebase > 1.0) {
+        fps = frame / (time - timebase);
+        timebase = time;
+        frame = 0;
+
+        fpsList.push_back(fps);
+      }
+
+      runCUDA();
+
+      std::ostringstream ss;
+      ss << "[";
+      ss.precision(1);
+      ss << std::fixed << fps;
+      ss << " fps] " << deviceName;
+      glfwSetWindowTitle(window, ss.str().c_str());
+
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+      #if VISUALIZE
+      glUseProgram(program[PROG_BOID]);
+      glBindVertexArray(boidVAO);
+      glPointSize((GLfloat)pointSize);
+      glDrawElements(GL_POINTS, N_FOR_VIS + 1, GL_UNSIGNED_INT, 0);
+      glPointSize(1.0f);
+
+      glUseProgram(0);
+      glBindVertexArray(0);
+
+      glfwSwapBuffers(window);
+      #endif
+    }
+
+    int numIgnore = 10;
+    if (fpsList.size() > numIgnore) {
+      std::vector<double> newFpsList(fpsList.begin() + numIgnore, fpsList.end());
+
+      double averageFps = 0;
+      for (auto fps : newFpsList)
+        averageFps += fps;
+      averageFps /= newFpsList.size();
+
+      std::cout << "Average FPS: " << averageFps << std::endl;
+    }
+    
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+
+
+  void errorCallback(int error, const char *description) {
+    fprintf(stderr, "error %d: %s\n", error, description);
+  }
+
+  void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+  }
+
+  void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    leftMousePressed = (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS);
+    rightMousePressed = (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS);
+  }
+
+  void mousePositionCallback(GLFWwindow* window, double xpos, double ypos) {
+    if (leftMousePressed) {
+      // compute new camera parameters
+      phi += (xpos - lastX) / width;
+      theta -= (ypos - lastY) / height;
+      theta = std::fmax(0.01f, std::fmin(theta, 3.14f));
+      updateCamera();
+    }
+    else if (rightMousePressed) {
+      zoom += (ypos - lastY) / height;
+      zoom = std::fmax(0.1f, std::fmin(zoom, 5.0f));
+      updateCamera();
+    }
+
+	lastX = xpos;
+	lastY = ypos;
+  }
+
+  void updateCamera() {
+    cameraPosition.x = zoom * sin(phi) * sin(theta);
+    cameraPosition.z = zoom * cos(theta);
+    cameraPosition.y = zoom * cos(phi) * sin(theta);
+    cameraPosition += lookAt;
+
+    projection = glm::perspective(fovy, float(width) / float(height), zNear, zFar);
+    glm::mat4 view = glm::lookAt(cameraPosition, lookAt, glm::vec3(0, 0, 1));
+    projection = projection * view;
+
+    GLint location;
+
+    glUseProgram(program[PROG_BOID]);
+    if ((location = glGetUniformLocation(program[PROG_BOID], "u_projMatrix")) != -1) {
+      glUniformMatrix4fv(location, 1, GL_FALSE, &projection[0][0]);
+    }
+  }
