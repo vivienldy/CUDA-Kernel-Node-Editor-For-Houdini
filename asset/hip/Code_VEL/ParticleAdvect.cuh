a31f8d9f0999449bbc60fdb3ba79033cb79ba1a8
Dongying Liu <vivienliu1998@gmail.com>
2022-12-09 23:54:50 -0500

    particle advect vop change

--- asset/hip/Code_VEL/ParticleAdvect.cuh
+++ asset/hip/Code_VEL/ParticleAdvect.cuh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #include <cuda.h> 
-=======
-#include <cuda.h>
->>>>>>> c6697ae8c39c9fb59de35b574ac2e713d9e084dd
 #include <glm/glm.hpp>
 #include <device_launch_parameters.h>
 
@@ -10,21 +6,6 @@ namespace CodeGenerator
 {
   namespace CUDAKernel
   {
-<<<<<<< HEAD
       __global__ void ParticleAdvect(glm::vec3 geo1_ParticleAdvect_offset_offset, float geo1_ParticleAdvect_input3_input3, float geo1_ParticleAdvect_input2_input2, glm::vec3* geo1_ParticleAdvect_geometryvopglobal1_Pbuffer, float geo1_ParticleAdvect_geometryvopglobal1_age, glm::vec3* geo1_ParticleAdvect_geometryvopglobal1_Cdbuffer, float geo1_ParticleAdvect_geometryvopglobal1_TimeInc, CGGeometry::RAWData geo1_ParticleAdvect_geometryvopglobal1_OpInput1, CGGeometry::RAWData geo1_ParticleAdvect_geometryvopglobal1_OpInput2, float* __geo1_ParticleAdvect_add1_sum_debug_buffer,  int numThreads);
-=======
-    __global__ void ParticleAdvect(
-      glm::vec3 geo1_ParticleAdvect_offset_offset, 
-      float geo1_ParticleAdvect_input3_input3, 
-      float geo1_ParticleAdvect_input2_input2, 
-      glm::vec3 *geo1_ParticleAdvect_geometryvopglobal1_Pbuffer, 
-      float geo1_ParticleAdvect_geometryvopglobal1_age, 
-      glm::vec3 *geo1_ParticleAdvect_geometryvopglobal1_Cdbuffer, 
-      float geo1_ParticleAdvect_geometryvopglobal1_TimeInc, 
-      CGGeometry::RAWData geo1_ParticleAdvect_geometryvopglobal1_OpInput1, 
-      CGGeometry::RAWData geo1_ParticleAdvect_geometryvopglobal1_OpInput2, 
-      float *__geo1_ParticleAdvect_add1_sum_debug_buffer, 
-      int numThreads);
->>>>>>> c6697ae8c39c9fb59de35b574ac2e713d9e084dd
   }
 }
